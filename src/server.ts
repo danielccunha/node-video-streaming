8a1078bcc4248715e2c50170321073d0d9ea379a
Daniel Cunha <danielcunha54@gmail.com>
2021-08-10 20:52:54 -0300

    chore: setup express app and add sample video

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log("Hi there");
+import express from "express";
+import morgan from "morgan";
+import chalk from "chalk";
+
+import { routes } from "./routes";
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+app.use(morgan("dev"));
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${chalk.yellow(port)}`);
+});
